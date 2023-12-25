db724993e2b7aebfdcb966c744986d778b88089e
Mike Slinn <mslinn@mslinn.com>
2023-12-24 20:08:36 -0500

    -

--- lib/options.rb
+++ lib/options.rb
@@ -1,6 +1,8 @@
 require 'colorator'
 require 'optparse'
 
+VERBOSITY = %w[trace debug verbose info warning error fatal panic quiet].freeze
+
 def help(msg = nil)
   printf "Error: #{msg}\n\n".yellow unless msg.nil?
   msg = <<~END_HELP
@@ -16,7 +18,7 @@ def help(msg = nil)
       -f Overwrite output file if present
       -h Show this help message
       -s Shakiness compensation 1..10 (default 5)
-      -v Verbosity -loglevel error -stats
+      -v Verbosity; one of: #{VERBOSITY.join ', '}
       -z Zoom percentage (computed if not specified)
 
     See:
@@ -38,6 +40,7 @@ def parse_options
       help
     end
     parser.on('-s', '--shake SHAKE', Integer, 'Shakiness (1..10)')
+    parser.on('-v', '--verbose VERBOSE', 'Zoom percentage')
     parser.on('-z', '--zoom ZOOM', Integer, 'Zoom percentage')
 
     parser.on_tail('-h', '--help', 'Show this message') do
@@ -45,6 +48,7 @@ def parse_options
       exit
     end
   end.parse!(into: options)
+  help "Invalid verbosity value (#{options[:verbose]}), must be one of one of: #{VERBOSITY.join ', '}." unless options[:verbose] in VERBOSITY
   help "Invalid shake value (#{options[:shake]})." if options[:shake].negative? || options[:shake] > 10
   options
 end
